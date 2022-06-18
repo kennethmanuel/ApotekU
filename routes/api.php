2d0e0504772d24fe4b72b9e0d9d7e3916307dd47
Kenneth Manuel <kennethlieyanto99@gmail.com>
2022-06-19 01:34:45 +0700

    Added auth using sanctum to api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,11 +17,33 @@ use App\Http\Controllers\MedicineController;
 |
 */
 
-Route::apiResource('medicines', MedicineController::class);
+// Route::apiResource('medicines', MedicineController::class);
+// Route::apiResource('categories', CategoryController::class);
+
+// Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/medicines', [MedicineController::class, 'index']);
+Route::get('/medicines/{id}', [MedicineController::class, 'show']);
 Route::get('/medicines/search/{generic_name}', [MedicineController::class, 'search']);
 
-Route::apiResource('categories', CategoryController::class);
+Route::get('/category', [CategoryController::class, 'index']);
+Route::get('/category/{id}', [CategoryController::class, 'show']);
 Route::get('/categories/search/{name}', [CategoryController::class, 'search']);
+
+// Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/medicines', [MedicineController::class, 'store']);
+    Route::put('/medicines/{id}', [MedicineController::class, 'update']);
+    Route::delete('/medicines/{id}', [MedicineController::class, 'destory']);
+
+    Route::post('/category', [CategoryController::class, 'store']);
+    Route::put('/category/{id}', [CategoryController::class, 'update']);
+    Route::delete('/category/{id}', [CategoryController::class, 'destory']);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
