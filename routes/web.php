6195617c36dcdc004a53d64e5b455bbd9c4d0199
Kenneth Manuel <kennethlieyanto99@gmail.com>
2022-06-19 13:11:54 +0700

    Added create medicine page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MedicineController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/admin/medicine/create', [MedicineController::class, 'create'])->name('admin.medicine.create');
+Route::post('/admin/medicine/store', [MedicineController::class, 'store'])->name('admin.medicine.store');
 
 Auth::routes();
 
