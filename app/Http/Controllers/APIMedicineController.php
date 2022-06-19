2f28533685754ccedeb50900da68c93450c6ede5
Kenneth Manuel <kennethlieyanto99@gmail.com>
2022-06-19 12:20:42 +0700

    Rename API controllers

--- app/Http/Controllers/APIMedicineController.php
+++ app/Http/Controllers/APIMedicineController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Medicine;
 use Illuminate\Http\Request;
 
-class MedicineController extends Controller
+class APIMedicineController extends Controller
 {
     /**
      * Display a listing of the resource.
